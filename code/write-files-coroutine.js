a571e5bf84ab1fffd7c83bb07a267766f64f5b4c
Feihong <feihong.hsu@gmail.com>
2017-01-03 22:59:27 -0600

    Print a message before every write and print a final message upon completion

--- code/write-files-coroutine.js
+++ code/write-files-coroutine.js
@@ -5,7 +5,9 @@ let fileNames = ['a.txt', 'b.txt', 'c.txt']
 
 Promise.coroutine(function *() {
   for (let i=0; i < fileNames.length; i++) {
+    console.log(i + ' foobar')
     yield fs.writeFileAsync(fileNames[i], i + ' foobar')
     yield Promise.delay(1000)
   }
+  console.log('Done!')
 })()
--- code/write-files-promise.js
+++ code/write-files-promise.js
@@ -3,5 +3,7 @@ const fs = Promise.promisifyAll(require('fs'))
 
 let fileNames = ['a.txt', 'b.txt', 'c.txt']
 Promise.each(fileNames, (fileName, i) => {
+  console.log(i + ' foobar')
   return fs.writeFileAsync(fileName, i + ' foobar').delay(1000)
 })
+.then(() => console.log('Done!'))
