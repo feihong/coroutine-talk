ef40b7a734d15586471019312dd37604e4b397ec
Feihong Hsu <feihong.hsu@gmail.com>
2017-01-05 10:12:00 -0600

    Add logic for movies endpoint

--- movie-demo/server.js
+++ movie-demo/server.js
@@ -1,17 +1,32 @@
 const koa = require('koa')
 const serve = require('koa-static')
 const route = require('koa-route')
+const axios = require('axios')
 const app = koa()
 
 
 const GRACENOTE_API_KEY = process.env.GRACENOTE_API_KEY
-
+const GRACENOTE_URL = 'http://data.tmsapi.com/v1.1/movies/showings'
 
 app.use(route.get('/movies/', movies))
 app.use(route.get('/ratings/', ratings))
 
 function *movies() {
-  this.body = ["a", "b", "c"]
+  let {lat, lng} = this.request.query
+  let params = {
+    startDate: moment().format('YYYY-MM-DD'),
+    api_key: GRACENOTE_API_KEY
+    lat,
+    lng,
+  }
+  let movies = yield axios.get(url, {params}).then(res => res.data)
+  this.body = movies.map(movie => {
+    return {
+      title: movie.title,
+      year: movie.releaseYear,
+      genres: movie.genres,
+    }
+  })
 }
 
 function *ratings() {
