a2222f2297a8045cba6c62f0326eda47c1f42a53
Feihong Hsu <feihong.hsu@gmail.com>
2017-01-04 22:42:38 -0600

    Add api endpoints

--- movie-demo/server.js
+++ movie-demo/server.js
@@ -1,10 +1,22 @@
 const koa = require('koa')
 const serve = require('koa-static')
+const route = require('koa-route')
 const app = koa()
 
-app.use(function *(){
-  this.body = 'Hello World'
-})
+
+const GRACENOTE_API_KEY = process.env.GRACENOTE_API_KEY
+
+
+app.use(route.get('/movies/', movies))
+app.use(route.get('/ratings/', ratings))
+
+function *movies() {
+  this.body = ["a", "b", "c"]
+}
+
+function *ratings() {
+  this.body = ["a", "b", "c"]
+}
 
 app.use(serve('.'))
 
