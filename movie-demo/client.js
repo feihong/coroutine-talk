09205940ebf4e7b1c7f412c9b31c1b62d8482b16
Feihong <feihong.hsu@gmail.com>
2017-01-04 23:11:51 -0600

    Get the location on page load

--- movie-demo/client.js
+++ movie-demo/client.js
@@ -1,1 +1,22 @@
-console.log('hello')
+$(document).ready(() => {
+//=============================================================================
+
+Promise.coroutine(function *() {
+  let coords = yield getCurrentPosition()
+  console.log(coords)
+  $('.location').text(`Your location is (${coords.latitude}, ${coords.longitude})`)
+})()
+
+
+
+function getCurrentPosition() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      position => resolve(position.coords),
+      error => reject(error)
+    )
+  })
+}
+
+//=============================================================================
+})
