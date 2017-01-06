8e2c0ab379ca23d1182a4e5bfc68b847c4881f7d
Feihong Hsu <feihong.hsu@gmail.com>
2017-01-05 22:18:25 -0600

    Add fake request handler functions that don't hit the apis

--- movie-demo/client.js
+++ movie-demo/client.js
@@ -6,7 +6,21 @@ Promise.coroutine(function *() {
   // console.log(coords)
   $('.location').text(`Your location is (${coords.latitude}, ${coords.longitude})`)
   let movies = yield $.getJSON('/movies', {lat: coords.latitude, lng: coords.longitude})
-  console.log(movies)
+  console.log(movies[0])
+  for (let movie of movies) {
+    let data = yield $.getJSON(
+      '/rating', {title: movie.title, year: movie.year})
+    movie.rating = data.rating
+  }
+  debugger
+  movies.sort((a, b) => b.rating - a.rating)
+  for (let movie of movies) {
+    $(`<div>
+        <strong>${movie.title}</strong>,
+        rating: ${movie.rating},
+        next showtime: ${movie.showtime}
+      </div>`).appendTo('.movies')
+  }
 })()
 
 
--- movie-demo/server.js
+++ movie-demo/server.js
@@ -8,9 +8,13 @@ const app = koa()
 
 const GRACENOTE_API_KEY = process.env.GRACENOTE_API_KEY
 const GRACENOTE_URL = 'http://data.tmsapi.com/v1.1/movies/showings'
+const OMDB_URL = 'http://omdbapi.com/'
 
-app.use(route.get('/movies/', movies))
-app.use(route.get('/ratings/', ratings))
+// app.use(route.get('/movies/', movies))
+// app.use(route.get('/rating/', rating))
+app.use(route.get('/movies/', fakeMovies))
+app.use(route.get('/rating/', fakeRating))
+
 
 function *movies() {
   let {lat, lng} = this.request.query
@@ -20,19 +24,53 @@ function *movies() {
     lat,
     lng,
   }
-  let movies = yield axios.get(GRACENOTE_URL, {params}).then(res => res.data)
+  let res = yield axios.get(GRACENOTE_URL, {params})
+  this.body = res.data.map(movie => {
+    return {
+      title: movie.title,
+      year: movie.releaseYear,
+      genres: movie.genres,
+      showtime: movie.showtimes[0].dateTime,
+    }
+  })
+}
+
+function *rating() {
+  let {title, year} = this.request.query
+  let params = {
+    t: title,
+    y: year,
+    type: 'movie',
+    tomatoes: 'true'
+  }
+  let res = yield axios.get(OMDB_URL, {params})
+  let movie = res.data
+  let avgRating = (
+    (parseInt(movie.Metascore) / 10) +
+    parseFloat(movie.imdbRating) +
+    parseFloat(movie.tomatoRating)
+  ) / 3
+  this.body = {rating: avgRating}
+}
+
+function *fakeMovies() {
+  const fs = require('fs')
+  let data = yield new Promise(resolve => {
+    fs.readFile('gracenote-response.json', (err, data) => resolve(data))
+  })
+  let movies = JSON.parse(data)
   this.body = movies.map(movie => {
     return {
       title: movie.title,
       year: movie.releaseYear,
       genres: movie.genres,
-      showtime: movie.showtimes[0],
+      showtime: movie.showtimes[0].dateTime,
     }
   })
 }
 
-function *ratings() {
-  this.body = ["a", "b", "c"]
+function *fakeRating() {
+  this.body = {rating: Math.floor(Math.random() * 100) / 10}
 }
 
 app.use(serve('.'))
