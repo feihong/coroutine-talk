309375833f4bddb031ab5cb7c7ae850cbd287e8c
Feihong Hsu <feihong.hsu@gmail.com>
2017-01-06 22:10:53 -0600

    Add live coding start file

---
+++ live-coding/write-files-start.js
@@ -0,0 +1,16 @@
+const Promise = require('bluebird')
+const fs = Promise.promisifyAll(require('fs'))
+
+let fileNames = ['a.txt', 'b.txt', 'c.txt']
+
+function *writeFiles() {
+  for (let i=0; i < fileNames.length; i++) {
+    console.log(i + ' foobar')
+    yield fs.writeFileAsync(fileNames[i], i + ' foobar')
+    console.log(i + ' sleep')
+    yield Promise.delay(1000)
+  }
+  console.log('Done!')
+}
+
+let generator = writeFiles()
