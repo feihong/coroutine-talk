48b83821c7d0e5329414484c5f1b62d789dc9673
Feihong Hsu <feihong.hsu@gmail.com>
2017-01-08 11:47:04 -0600

    Use fs-promise instead of promisifying fs

--- code/write-files-promise.js
+++ code/write-files-promise.js
@@ -1,10 +1,11 @@
-const Promise = require('bluebird')
-const fs = Promise.promisifyAll(require('fs'))
+const bluebird = require('bluebird')
+const fs = require('fs-promise')
 
 let fileNames = ['a.txt', 'b.txt', 'c.txt']
 
-Promise.each(fileNames, (fileName, i) => {
+bluebird.each(fileNames, (fileName, i) => {
   console.log(i + ' foobar')
-  return fs.writeFileAsync(fileName, i + ' foobar').delay(1000)
+  return fs.writeFile(fileName, i + ' foobar')
+  .then(() => bluebird.delay(1000))
 })
 .then(() => console.log('Done!'))
